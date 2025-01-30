4ef3208febbfd908c2f9585791df80bdb695958b
mamedbekov <mamedbekovruslan@yandex.ru>
2025-01-30 13:50:24 +0300

    Добавить обработчик корсов

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./src/routes/index.js";
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
@@ -14,8 +18,6 @@ app.get(
   },
   router
 );
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
